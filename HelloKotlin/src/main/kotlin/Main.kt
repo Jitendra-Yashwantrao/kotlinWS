85da652bbd7bf9dd8869a0aea9937df8ea628d57
jitendra yashwantrao <jitendray@Jitendra-Personal.local>
2022-12-29 15:25:42 +0530

    spread operator for array

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main(args: Array<String>) {
-    var interestingThings = listOf("kotlin", "java", "golang")
+    var interestingThings = arrayOf("kotlin", "java", "golang")
     sayGreeting("hello how are you")
     sayGreeting("hello", "kotlin", "java", "golang")
+    sayGreeting("spread operator", *interestingThings)
 
 }
 
