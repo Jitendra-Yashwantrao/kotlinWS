b2d0d403d2eab7a4ae313de969f12c1a178cd2f2
jitendra yashwantrao <jitendray@Jitendra-Personal.local>
2022-12-29 12:07:27 +0530

    parameterised function

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -1,25 +1,6 @@
-var name: String? = "Jitendra"
-
-
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
-    when (name) {
-        "test" ->
-            println("name: ${name} ")
-
-        "jitendra" ->
-            println("jitendra case")
-
-        else ->
-            println("case not found")
-    }
-
-    println(getGreeting())
+    sayGreeting("hell", "world")
+    sayGreeting("Hey", "kotlin")
 }
 
-fun getGreeting() = "hello"
+fun sayGreeting(greeting: String, textToGreet: String) = println("$greeting - $textToGreet")
