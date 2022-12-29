0f991a882d4d904e961999eb7ca8e225a526f09a
jitendra yashwantrao <jitendray@Jitendra-Personal.local>
2022-12-29 13:07:06 +0530

    foreach indexed

--- HelloKotlin/src/main/kotlin/Main.kt
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -7,7 +7,19 @@ fun main(args: Array<String>) {
         println(interestingThing)
     }
 
-    println(interestingThings.toString())
+    println("other way")
+
+    interestingThings.forEach {
+        println(it)
+    }
+
+    println("one more way")
+
+    interestingThings.forEach { interestingThings -> println(interestingThings) }
+
+    println("one more way to print index")
+
+    interestingThings.forEachIndexed { index, interestingThing -> println("$interestingThing is at index $index") }
 }
 
 fun sayGreeting(greeting: String, textToGreet: String) = println("$greeting - $textToGreet")
